cfa5ee99b30a8393f47a98951230dc76bb8c6da6
socklee416 <saj72@pitt.edu>
2017-04-01 02:11:33 -0400

    Added some Wavespawner stuff (totally ripped)

---
+++ Shawn/Dicking Around with 1666 Final Stuff/Assets/_Scripts/Tower_Builder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_Builder : MonoBehaviour
+{
+
+    public GameObject Tower_Prefab;
+    private Tower_Director Director;
+    private RaycastHit hit;
+    private int layerMask;
+    private static float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Director = GameObject.Find("Tower Controller").GetComponent<Tower_Director>();
+        timer = 0;
+        //layerMask = (1 << 8);
+        layerMask |= (1 << 9);
+        layerMask |= (1 << 10);
+        layerMask = ~layerMask;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && timer <= 0)
+        {
+            timer = .5f;
+            if (Physics.Raycast(transform.position, -Vector3.up, out hit, 3f, layerMask))
+            {
+                if (hit.collider.gameObject.layer != 8)
+                {
+                    GameObject tower = Instantiate(Tower_Prefab, new Vector3(transform.position.x, 1f, transform.position.z), Quaternion.identity) as GameObject;
+                    Director.Add_Tower(tower);
+                }
+            }
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        } 
+    }
+
+}
---
+++ Shawn/Dicking Around with 1666 Final Stuff/Assets/_Scripts/Tower_Movement_Listener.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_Movement_Listener : MonoBehaviour
+{
+
+    private Transform t;
+    private GameObject Controller;
+    public GameObject ID_Band;
+    public Material Team_1_Mat, Team_2_Mat, Team_3_Mat, Team_4_Mat;
+    public int Twr_Team;
+
+    // Use this for initialization
+    void Start()
+    {
+        Controller = GameObject.Find("Tower Controller");
+        t = transform;
+
+        Twr_Team = Random.Range(1, 5);
+
+        if (Twr_Team == 1)
+            ID_Band.GetComponent<Renderer>().material = Team_1_Mat;
+        else if (Twr_Team == 2)
+            ID_Band.GetComponent<Renderer>().material = Team_2_Mat;
+        else if (Twr_Team == 3)
+            ID_Band.GetComponent<Renderer>().material = Team_3_Mat;
+        else if (Twr_Team == 4)
+            ID_Band.GetComponent<Renderer>().material = Team_4_Mat;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Twr_Team == Controller.GetComponent<Controller_Mouselook>().Team)
+        {
+            t.rotation = Controller.transform.rotation;
+        }
+    }
+}
