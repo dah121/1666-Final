1c8ab2f4cb1896e02749132f2648c1bb70813696
dah121 <dah121@pitt.edu>
2017-04-02 15:47:40 -0400

    Add existing file

---
+++ David/Dicking Around with 1666 Final Stuff/Assets/_Scripts/Prototype_Nav.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Prototype_Nav : MonoBehaviour {
+
+    public Transform Destination;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<NavMeshAgent>().destination = Destination.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ David/Dicking Around with 1666 Final Stuff/Assets/navi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navi : MonoBehaviour {
+
+    public GameObject dest;
+
+    // Use this for initialization
+    void Start () {
+        this.GetComponent<NavMeshAgent>().destination = dest.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
